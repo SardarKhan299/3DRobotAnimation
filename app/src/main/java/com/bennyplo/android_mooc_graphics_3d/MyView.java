06ec9279286b07bfc77652f4168b86b092affac1
Administrator <sardar@mypaperlessoffice.com>
2019-12-31 01:22:17 +0500

    Design left hand of the robo.

--- app/src/main/java/com/bennyplo/android_mooc_graphics_3d/MyView.java
+++ app/src/main/java/com/bennyplo/android_mooc_graphics_3d/MyView.java
@@ -8,7 +8,7 @@ import android.util.Log;
 import android.view.View;
 
 public class MyView extends View {
-    private Paint redPaint,bluePaint,pinkPaint; //paint object for drawing the lines
+    private Paint redPaint,bluePaint,pinkPaint,greenPaint,lightBluePaint; //paint object for drawing the lines
     private Coordinate[]cube_vertices;//the vertices of a 3D cube
     private Coordinate[]draw_cube_vertices;//the vertices for drawing a 3D cube
     public MyView(Context context) {
@@ -29,6 +29,17 @@ public class MyView extends View {
         pinkPaint.setStyle(Paint.Style.STROKE);//Stroke
         pinkPaint.setColor(Color.parseColor("#FF4081"));
         pinkPaint.setStrokeWidth(2);
+
+        greenPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        greenPaint.setStyle(Paint.Style.STROKE);//Stroke
+        greenPaint.setColor(Color.GREEN);
+        greenPaint.setStrokeWidth(2);
+
+        lightBluePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        lightBluePaint.setStyle(Paint.Style.STROKE);//Stroke
+        lightBluePaint.setColor(Color.parseColor("#5DD4E3"));
+        lightBluePaint.setStrokeWidth(2);
+
         //create a 3D cube
         cube_vertices = new Coordinate[8];
         cube_vertices[0] = new Coordinate(-1, -1, -1, 1);
@@ -70,7 +81,7 @@ public class MyView extends View {
         DrawLinePairs(canvas, draw_cube_vertices, 2, 6, bluePaint);
         DrawLinePairs(canvas, draw_cube_vertices, 3, 7, bluePaint);
     }
-    private void DrawCube1(Canvas canvas)
+    private void DrawCubeHead(Canvas canvas)
     {//draw a cube on the screen
         draw_cube_vertices=translate(cube_vertices,9,16,4);
         draw_cube_vertices=scale(draw_cube_vertices,40,20,0);
@@ -88,6 +99,60 @@ public class MyView extends View {
         DrawLinePairs(canvas, draw_cube_vertices, 3, 7, pinkPaint);
     }
 
+    private void DrawCubeLeftHand1(Canvas canvas)
+    {//draw a cube on the screen
+        draw_cube_vertices=translate(cube_vertices,2.7,5.9,0);
+        draw_cube_vertices=scale(draw_cube_vertices,50,70,0);
+        DrawLinePairs(canvas, draw_cube_vertices, 0, 1, bluePaint);
+        DrawLinePairs(canvas, draw_cube_vertices, 1, 3, bluePaint);
+        DrawLinePairs(canvas, draw_cube_vertices, 3, 2, bluePaint);
+        DrawLinePairs(canvas, draw_cube_vertices, 2, 0, bluePaint);
+        DrawLinePairs(canvas, draw_cube_vertices, 4, 5, bluePaint);
+        DrawLinePairs(canvas, draw_cube_vertices, 5, 7, bluePaint);
+        DrawLinePairs(canvas, draw_cube_vertices, 7, 6, bluePaint);
+        DrawLinePairs(canvas, draw_cube_vertices, 6, 4, bluePaint);
+        DrawLinePairs(canvas, draw_cube_vertices, 0, 4, bluePaint);
+        DrawLinePairs(canvas, draw_cube_vertices, 1, 5, bluePaint);
+        DrawLinePairs(canvas, draw_cube_vertices, 2, 6, bluePaint);
+        DrawLinePairs(canvas, draw_cube_vertices, 3, 7, bluePaint);
+    }
+
+    private void DrawCubeLeftHand2(Canvas canvas)
+    {//draw a cube on the screen
+        draw_cube_vertices=translate(cube_vertices,2.7,7.9,0);
+        draw_cube_vertices=scale(draw_cube_vertices,50,70,0);
+        DrawLinePairs(canvas, draw_cube_vertices, 0, 1, greenPaint);
+        DrawLinePairs(canvas, draw_cube_vertices, 1, 3, greenPaint);
+        DrawLinePairs(canvas, draw_cube_vertices, 3, 2, greenPaint);
+        DrawLinePairs(canvas, draw_cube_vertices, 2, 0, greenPaint);
+        DrawLinePairs(canvas, draw_cube_vertices, 4, 5, greenPaint);
+        DrawLinePairs(canvas, draw_cube_vertices, 5, 7, greenPaint);
+        DrawLinePairs(canvas, draw_cube_vertices, 7, 6, greenPaint);
+        DrawLinePairs(canvas, draw_cube_vertices, 6, 4, greenPaint);
+        DrawLinePairs(canvas, draw_cube_vertices, 0, 4, greenPaint);
+        DrawLinePairs(canvas, draw_cube_vertices, 1, 5, greenPaint);
+        DrawLinePairs(canvas, draw_cube_vertices, 2, 6, greenPaint);
+        DrawLinePairs(canvas, draw_cube_vertices, 3, 7, greenPaint);
+    }
+
+    private void DrawCubeLeftHand3(Canvas canvas)
+    {//draw a cube on the screen
+        draw_cube_vertices=translate(cube_vertices,2.7,32,0);
+        draw_cube_vertices=scale(draw_cube_vertices,50,20,0);
+        DrawLinePairs(canvas, draw_cube_vertices, 0, 1, lightBluePaint);
+        DrawLinePairs(canvas, draw_cube_vertices, 1, 3, lightBluePaint);
+        DrawLinePairs(canvas, draw_cube_vertices, 3, 2, lightBluePaint);
+        DrawLinePairs(canvas, draw_cube_vertices, 2, 0, lightBluePaint);
+        DrawLinePairs(canvas, draw_cube_vertices, 4, 5, lightBluePaint);
+        DrawLinePairs(canvas, draw_cube_vertices, 5, 7, lightBluePaint);
+        DrawLinePairs(canvas, draw_cube_vertices, 7, 6, lightBluePaint);
+        DrawLinePairs(canvas, draw_cube_vertices, 6, 4, lightBluePaint);
+        DrawLinePairs(canvas, draw_cube_vertices, 0, 4, lightBluePaint);
+        DrawLinePairs(canvas, draw_cube_vertices, 1, 5, lightBluePaint);
+        DrawLinePairs(canvas, draw_cube_vertices, 2, 6, lightBluePaint);
+        DrawLinePairs(canvas, draw_cube_vertices, 3, 7, lightBluePaint);
+    }
+
     private void DrawLine(Canvas canvas)
     {//draw a cube on the screen
         draw_cube_vertices=translate(cube_vertices,2,18,4);
@@ -101,9 +166,11 @@ public class MyView extends View {
         //draw objects on the screen
         super.onDraw(canvas);
         DrawCube(canvas);//draw a cube onto the screen
-        DrawCube1(canvas);
+        DrawCubeHead(canvas);
         DrawLine(canvas);
-
+        DrawCubeLeftHand1(canvas);
+        DrawCubeLeftHand2(canvas);
+        DrawCubeLeftHand3(canvas);
     }
     //*********************************
     //matrix and transformation functions
